42df1401c67e2167db67c9477f57f2bbd2a1029d
mcid-baq <133130064+mcid-baq@users.noreply.github.com>
2023-05-26 12:36:42 +0200

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcid-baq <mcidbaquerizo@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 10:56:20 by mcid-baq          #+#    #+#             */
+/*   Updated: 2023/05/25 13:55:49 by mcid-baq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *x);
+char	*ft_strjoin(char *next_line, char *temp);
+char	*ft_strchr(char *x, int y);
+
+#endif
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcid-baq <mcidbaquerizo@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 11:58:25 by mcid-baq          #+#    #+#             */
+/*   Updated: 2023/05/25 13:55:45 by mcid-baq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *x);
+char	*ft_strjoin(char *next_line, char *temp);
+char	*ft_strchr(char *x, int y);
+
+#endif
